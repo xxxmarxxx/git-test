e9285fbe2eefdf4a5a4c6fe9c8ee63f6c4b7682a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-10-21 23:46:05 +0200

    test2

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const header = document.querySelector("header");
-const h1 = document.querySelector('h1');
+const h1 = document.querySelector("h1");
 let image = true;
 let title = true;
 
-const changeImage = ()=> {
-    image = !image;
-    title = !title;
-    header.style.backgroundImage = image ? "url(test.jpg)" : "url(tst2.jpg)";
-    h1.textContent = title ? "Balony" : "Pszczoly"
-}
-header.addEventListener("click", changeImage)
+const changeImage = () => {
+  image = !image;
+  title = !title;
+  header.style.backgroundImage = image ? "url(test.jpg)" : "url(test1.png)";
+  h1.textContent = title ? "Balony" : "Pszczoly";
+};
+header.addEventListener("click", changeImage);
